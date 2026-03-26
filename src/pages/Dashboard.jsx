98d4a6b12b81d358118e42f1a559a905465b59f2
Basharathullah <bashara2272@gmail.com>
2026-03-27 00:14:37 +0530

    Minor changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -105,7 +105,7 @@ export default function Dashboard() {
                                 stroke="#94a3b8"
                                 tick={{ fontSize: 11 }}
                                 interval={0}
-                                angle={-30}
+                                angle={-10}
                                 textAnchor="end"
                                 height={60}
                             />
